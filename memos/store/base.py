1e32edf698976661bb8e966873875687d0e0080e
arrackisarookie <arrackisarookie@gmail.com>
2023-08-07 14:19:51 +0800

    [A] 配置信息使用类存储

--- memos/store/base.py
+++ memos/store/base.py
@@ -3,9 +3,10 @@ from datetime import datetime
 from databases import Database
 from sqlalchemy import MetaData, Table, Column, String, TIMESTAMP, TEXT, ForeignKey
 
-DATABASE_URL = "sqlite+aiosqlite:///resources/memos.db"
+from memos.config import get_settings
 
-db = Database(DATABASE_URL)
+settings = get_settings()
+db = Database(settings.DATABASE_URL)
 metadata = MetaData()
 
 
@@ -27,6 +28,17 @@ Memo = Table(
     Column("id", String, primary_key=True),
     Column("content", TEXT),
     Column("user_id", None, ForeignKey("users.id")),
+    Column("created_at", TIMESTAMP, default=datetime.now),
+    Column("updated_at", TIMESTAMP, default=datetime.now),
+    Column("deleted_at", TIMESTAMP, default=datetime.now)
+)
+
+Comment = Table(
+    "comments",
+    metadata,
+    Column("id", String, primary_key=True),
+    Column("content", TEXT),
+    Column("memo_id", None, ForeignKey("memos.id")),
     Column("created_at", TIMESTAMP, default=datetime.now),
     Column("updated_at", TIMESTAMP, default=datetime.now),
     Column("deleted_at", TIMESTAMP, default=datetime.now)
